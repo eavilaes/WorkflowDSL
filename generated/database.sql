253d6532aa2af58e5ef7084e35d1cf7067fbb35f
Eric <ericavilaescudero@gmail.com>
2019-06-21 14:58:02 +0200

    SQL File

---
+++ generated/database.sql
@@ -0,0 +1,12 @@
+CREATE TABLE Selectapizza (
+	typeofpizzaform VARCHAR,
+);
+CREATE TABLE Fillpersonalinfo (
+	PersonalData VARCHAR,
+);
+CREATE TABLE Satisfactionquestionnaire (
+	SatisfactionDegree VARCHAR,
+);
+CREATE TABLE UploadDocuments (
+	Documents VARCHAR,
+);
