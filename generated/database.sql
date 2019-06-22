7c8724eaf82ac2fe9bb9e77d7e77ebf20002d058
Eric <ericavilaescudero@gmail.com>
2019-06-22 12:50:27 +0200

    Updated SQL generation

--- generated/database.sql
+++ generated/database.sql
@@ -1,12 +1,20 @@
 CREATE TABLE Selectapizza (
+	id INT AUTO_INCREMENT,
+	PRIMARY KEY (id)
 );
 CREATE TABLE Fillpersonalinfo (
+	id INT AUTO_INCREMENT,
 	Name VARCHAR,
 	Address VARCHAR,
 	CreditCard VARCHAR,
+	PRIMARY KEY (id)
 );
 CREATE TABLE Satisfactionquestionnaire (
+	id INT AUTO_INCREMENT,
 	Satisfactiondegree VARCHAR,
+	PRIMARY KEY (id)
 );
 CREATE TABLE UploadDocuments (
+	id INT AUTO_INCREMENT,
+	PRIMARY KEY (id)
 );
